349875faaa2c0fbd073559d04223e6d4e66bad41
rupa <rupashah9055@gmail.com>
2024-01-13 16:25:30 +0530

    13/01/2024 changes done

--- MediTechSolution_mainProject.API/Model/Slots.cs
+++ MediTechSolution_mainProject.API/Model/Slots.cs
@@ -1,7 +1,11 @@
-﻿namespace MediTechSolution_mainProject.API.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MediTechSolution_mainProject.API.Model
 {
     public class Slots
     {
+        [Key]
         public int Id { get; set; }
         public DateTime Date { get; set; }
 
